e8ab7afd943d539bd5f5f37451267e31045a79cc
goatcheese82 <nonsquishy@gmail.com>
2019-02-21 16:49:08 -0600

    added login component

---
+++ src/components/logIn.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class logIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            credentials: {
+                email: '',
+                password: ''
+            }
+        }
+    }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
